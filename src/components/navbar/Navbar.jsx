1e9177ba3b2c7cb23cfea79296285e7239911252
JonathanStillman <68572893+JonathanStillman@users.noreply.github.com>
2023-12-11 11:07:04 -0600

    Create Navbar.jsx

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+// Navbar Styles Import
+import "./navbar.scss"
+
+// Material-UI Imports
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+
+// Dark Mode
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" style={{cursor: "pointer"}} onClick={() => dispatch({ type: "TOGGLE" })} />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <FormatListBulletedOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
